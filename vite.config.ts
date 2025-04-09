6a3796d51f0b9d7a24f4d87cbcbacbff7007afe7
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-04-09 13:55:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
-  server: {
-		allowedHosts: "devserver-main--superb-pegasus-37126a.netlify.app"
-	},
+  base: "./"
 });
